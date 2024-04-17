d5be850e6298faffca9061618cfd234374855d6a
ghdghdghd <ghdtmdgus3641@gmail.com>
2024-04-17 22:56:50 +0900

    user update

--- hhplus-ECommerce/src/main/java/io/hhplus/ECommerce/user/domain/UserRepository.java
+++ hhplus-ECommerce/src/main/java/io/hhplus/ECommerce/user/domain/UserRepository.java
@@ -2,8 +2,18 @@ package io.hhplus.ECommerce.user.domain;
 
 import java.util.Optional;
 
+import io.hhplus.ECommerce.user.controller.UserResponse;
+
 public interface UserRepository {
 
-	public User findById(Long userId); 
+	public User findById(Long userId);
 
+	//유저 포인트 조회
+	public UserResponse pointInquiry(Long userId);
+	
+	//유저 포인트 차감
+	public void deductPoint(Long userId, int orderPrice); 
+
+	
+	
 }
